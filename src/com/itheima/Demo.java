d3242bebf180388957475ed8cb0b19586051af84
syshello <serrHello@163.com>
2018-06-03 21:40:52 +0800

    first commit

---
+++ src/com/itheima/Demo.java
@@ -0,0 +1,17 @@
+package com.itheima;
+
+/**
+ * @author 子龙
+ * @version 1.0
+ * @title 标题
+ * @date 2018/6/3
+ * @package com.itheima
+ * @company www.jgl.cn
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
